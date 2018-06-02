bf56bb913235f149b87dad526d108ebbf39d50e6
sumrabbit <arek.radziewicz@gmail.com>
2018-06-02 22:13:51 +0200

    initial map functionality

--- public/js/home.js
+++ public/js/home.js
@@ -3,12 +3,14 @@ var handleSignedInUser = function(user) {
     document.getElementById('userName').textContent = "Witaj " + firebase.auth().currentUser.email;
     document.getElementById('logowanie').style.display = 'none';
     document.getElementById('wyloguj').style.display = "inline-block";
+	document.getElementById('mapa').style.display = "inline-block";
 };
 
 var handleSignedOutUser = function() {
     document.getElementById('userName').style.display = 'none';
     document.getElementById('logowanie').style.display = "inline-block";
     document.getElementById('wyloguj').style.display = 'none';
+	document.getElementById('mapa').style.display = "none";
 };
 
 firebase.auth().onAuthStateChanged(function(user) {
