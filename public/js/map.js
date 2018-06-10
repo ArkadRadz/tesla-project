7577c16ab21c374fbeb825ee34daa4f2200a3baa
Arkham <arkham195@gmail.com>
2018-06-10 15:31:13 +0200

    map markers fix

--- public/js/map.js
+++ public/js/map.js
@@ -1,28 +1,28 @@
+// initMap is initialized by Map API so it's not called from 'window.addEventListener(...)'
 function initMap() {
 	var myLatLng = { lat: -25.363, lng: 131.044 };
-
 	var map = new google.maps.Map(document.getElementById('map'), {
 		zoom: 4,
 		center: myLatLng
 	});
-
 	var marker = new google.maps.Marker({
 		position: myLatLng,
-		title: 'Hello World!'
 	});
-
 	marker.setMap(map);
+	initDB(map);
 }
-
-function initDB() {
+function initDB(mapRef) {
 	var ref = firebase.database().ref();
-
 	ref.on('value',function(snap) {
-		console.log(snap.val());
+		markersArray = snap.val().chargingPoints;
+		markersArray.forEach(markerElement => {
+			// console.log(markerElement);
+			var newMarker = new google.maps.Marker({
+				position: markerElement,
+			});
+			newMarker.setMap(mapRef);
+		});
 	},function(error) {
-		console.log(error.)
+		console.log("Error: " + error.code)
 	});
 }
-
-
-window.addEventListener('load',initDB);
