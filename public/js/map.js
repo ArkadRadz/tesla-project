89b192c8d8e7be6343d7cae3c7eeecefe2024485
Arkham <arkham195@gmail.com>
2018-06-10 15:18:16 +0200

    init

--- public/js/map.js
+++ public/js/map.js
@@ -1,25 +1,28 @@
 function initMap() {
-    var uluru = {lat: 10, lng: 10};
-    map = new google.maps.Map(
-        document.getElementById('map'), {zoom: 15, center: uluru});
-    var marker = new google.maps.Marker({position: uluru, map: map});
+	var myLatLng = { lat: -25.363, lng: 131.044 };
 
-    var markerDatabase = 0;
-    var ref = firebase.database().ref();
-    ref.on("value", function(snapshot) {
-        markerDatabase = snapshot.val();
-    }, function (error) {
-        console.log("Error: " + error.code);
-    });
-    var markerArray = markerDatabase.chargingPoints;
-    for (var i = 0; i < markerDatabase.chargingPoints.length; i++) {
-        marker = new google.maps.Marker({
-            position: new google.maps.LatLng(markerArray[i].lat, markerArray[i].lng),
-            map: map
-        })
-    }
+	var map = new google.maps.Map(document.getElementById('map'), {
+		zoom: 4,
+		center: myLatLng
+	});
+
+	var marker = new google.maps.Marker({
+		position: myLatLng,
+		title: 'Hello World!'
+	});
+
+	marker.setMap(map);
 }
 
-document.addEventListener('DOMContentLoaded', function() {
-    initMap();
-}, false);
+function initDB() {
+	var ref = firebase.database().ref();
+
+	ref.on('value',function(snap) {
+		console.log(snap.val());
+	},function(error) {
+		console.log(error.)
+	});
+}
+
+
+window.addEventListener('load',initDB);
