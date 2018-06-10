7f5966abb7a8acffca9e5d7b5a74de5e04e94f60
sumrabbit <arek.radziewicz@gmail.com>
2018-06-10 03:28:26 +0200

    Reading markers from database

--- public/js/map.js
+++ public/js/map.js
@@ -1,4 +1,9 @@
 function initMap() {
+    var uluru = {lat: 10, lng: 10};
+    map = new google.maps.Map(
+        document.getElementById('map'), {zoom: 15, center: uluru});
+    var marker = new google.maps.Marker({position: uluru, map: map});
+
     var markerDatabase = 0;
     var ref = firebase.database().ref();
     ref.on("value", function(snapshot) {
@@ -6,13 +11,8 @@ function initMap() {
     }, function (error) {
         console.log("Error: " + error.code);
     });
-    var markerArray  = markerDatabase.chargingPoints;
-    var uluru = {lat: 10, lng: 10};
-    map = new google.maps.Map(
-        document.getElementById('map'), {zoom: 15, center: uluru});
-    var marker = new google.maps.Marker({position: uluru, map: map});
-
-    for (i = 0; i < markerDatabase.chargingPoints.length; i++) {
+    var markerArray = markerDatabase.chargingPoints;
+    for (var i = 0; i < markerDatabase.chargingPoints.length; i++) {
         marker = new google.maps.Marker({
             position: new google.maps.LatLng(markerArray[i].lat, markerArray[i].lng),
             map: map
