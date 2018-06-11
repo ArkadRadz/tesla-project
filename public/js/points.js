0bb944713aebbe119c5692ced237f91949a13526
Arkham <arkham195@gmail.com>
2018-06-11 19:24:09 +0200

    point.js add tab in if

--- public/js/points.js
+++ public/js/points.js
@@ -1,25 +1,27 @@
-window.onload = function(){
-    var table = document.getElementById('table');
-    initDB();
+window.onload = function () {
+	var table = document.getElementById('table');
+	initDB();
 }
 function initDB() {
-    var ref = firebase.database().ref();
-    //console.log(firebase.database());
-    //console.log(ref);
-    ref.once('value',function(snap) {
-        console.log('xd');
-        markersArray = snap.val().chargingPoints;
-        var table = document.getElementById('table');
-        Object.keys(markersArray).map(keyName => {
-            var objVal = markersArray[keyName];
-            console.log(objVal)
-            if (firebase.auth().currentUser.uid==objVal.id_uslugodawcy){
-            var row = table.insertRow(0);
-            var cell = row.insertCell(0);
-            cell.innerHTML = 'Długość geograficzna: '+objVal.lat;
-            cell.innerHTML+= '<br>Szerokość geograficzna: '+objVal.lng;
-            }
-        })
-       
-    });
+	var ref = firebase.database().ref();
+	//console.log(firebase.database());
+	//console.log(ref);
+	ref.once('value', function (snap) {
+		console.log('xd');
+		markersArray = snap.val().chargingPoints;
+		var table = document.getElementById('table');
+
+
+		Object.keys(markersArray).map(keyName => {
+			var objVal = markersArray[keyName];
+			console.log(objVal)
+			if (firebase.auth().currentUser.uid == objVal.id_uslugodawcy) {
+				var row = table.insertRow(0);
+				var cell = row.insertCell(0);
+				cell.innerHTML = 'Długość geograficzna: ' + objVal.lat;
+				cell.innerHTML += '<br>Szerokość geograficzna: ' + objVal.lng;
+			}
+		})
+
+	});
 }
